7705d0291c3fc7d22b81e90970bca9180b68bf32
Baba Faye <fayebaba81@gmail.com>
2024-08-01 13:07:29 +0000

    reglage au niveau de la responsivité

--- fichier/main.js
+++ fichier/main.js
@@ -1,18 +1,18 @@
-// Header Scroll
-let nav = document.querySelector(".navbar");
-window.onscroll = function () {
-    if(document.documentElement.scrollTop > 20){
-        nav.classList.add("header-scrolled");
-    }else{
-        nav.classList.remove("header-scrolled");
+// // Header Scroll
+// let nav = document.querySelector(".navbar");
+// window.onscroll = function () {
+//     if(document.documentElement.scrollTop > 20){
+//         nav.classList.add("header-scrolled");
+//     }else{
+//         nav.classList.remove("header-scrolled");
 
-    }
-}
-// nav hide
-let navBar = document.querySelectorAll(".navbar-link");
-let navCollapse = document.querySelector(".navbar-collapse.collapse");
-navBar.forEach(function(a){
-    a.addEventListener("click",function(){
-        navCollapse.classList.remove("show");
-    })
-})
+//     }
+// }
+// // nav hide
+// let navBar = document.querySelectorAll(".navbar-link");
+// let navCollapse = document.querySelector(".navbar-collapse.collapse");
+// navBar.forEach(function(a){
+//     a.addEventListener("click",function(){
+//         navCollapse.classList.remove("show");
+//     })
+// })
